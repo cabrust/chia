a4755981c3c69333aed80f6e6374bb3aa1056a24
Clemens-Alexander Brust <clemens-alexander.brust@uni-jena.de>
2021-09-03 10:34:43 +0200

    quick-venv: Moved python version up

--- quick-venv.sh
+++ quick-venv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python3.8 -m venv venv
+python3.9 -m venv venv
 source venv/bin/activate
-python3.8 -m pip install -U pip setuptools
-python3.8 setup.py develop
+python3.9 -m pip install -U pip setuptools
+python3.9 setup.py develop
