cc82ccca5e6d8b32e3d321c36162f484d20bdc87
Clemens-Alexander Brust <clemens-alexander.brust@dlr.de>
2021-10-08 13:47:33 +0200

    test_extrapolators: Test actual output instead of just running experiments

--- tests/test_extrapolators.py
+++ tests/test_extrapolators.py
@@ -1,9 +1,10 @@
 import os
 
 import config as pcfg
+import networkx as nx
 import pytest
 
-from chia import containers, helpers, instrumentation
+from chia import containers, helpers, instrumentation, knowledge
 from chia.components import extrapolators
 from chia.knowledge import information_content
 
@@ -37,8 +38,44 @@ def test_extrapolators(extrapolator_name: str, ic_method_name: str):
     # This IoC container constructs all the necessary objects for our experiment
     experiment_container = containers.ExperimentContainer(config, outer_observable=obs)
 
-    # Run this experiment _without_ exception handling
-    experiment_container.runner.run()
+    # Get important objects from the container
+    knowledge_base: knowledge.KnowledgeBase = experiment_container.knowledge_base
+    extrapolator: extrapolators.Extrapolator = experiment_container.extrapolator
+    rgraph: nx.DiGraph = knowledge_base.get_hyponymy_relation_rgraph()
+
+    # Get all concepts
+    concept_uids = [concept.uid for concept in knowledge_base.concepts()]
+    imprecise_concept_uids = [
+        concept_uid
+        for concept_uid in concept_uids
+        if rgraph.out_degree[concept_uid] > 0
+    ]
+
+    for imprecise_concept_uid in imprecise_concept_uids:
+        # Set 1: All concepts that apply to imprecise_concept_uid
+        set_1 = set(nx.ancestors(rgraph, imprecise_concept_uid)).union(
+            set([imprecise_concept_uid])
+        )
+
+        # Set 2: All concepts that imprecise_concept_uid applies to
+        set_2 = set(nx.descendants(rgraph, imprecise_concept_uid)).union(
+            set([imprecise_concept_uid])
+        )
+
+        # Generate a somewhat realistic input
+        probabilities = {}
+        for concept_uid in concept_uids:
+            if concept_uid in set_1:
+                probabilities[concept_uid] = 1.0
+            elif concept_uid in set_2:
+                probabilities[concept_uid] = 0.2
+            else:
+                probabilities[concept_uid] = 0.0
+
+        result = extrapolator.extrapolate([(imprecise_concept_uid, probabilities)])[0]
+
+        # Since apply_ground_truth is true, this should always work. Either way, the prediction should never "disagree" with the ground truth
+        assert result in set_2, f"While testing for gt={imprecise_concept_uid}"
 
     # Make sure all the data is saved
     obs.send_shutdown(successful=True)
